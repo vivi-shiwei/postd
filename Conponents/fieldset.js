4953077347b83107656aa664dfeb389474ac71cc
vivi-shiwei <2546710985@qq.com>
2020-06-10 20:56:45 +0800

    6-10-21

--- Conponents/fieldset.js
+++ Conponents/fieldset.js
@@ -10,41 +10,6 @@ import {
 const fieldset = (props) => {
   return (
     <>
-      {/* <Box
-        as='header'
-        my='20px'
-        position='relative'
-        textAlign='center'
-        className='header-line'
-      >
-        <Box
-          as='span'
-          px='10px'
-          fontSize='35px'
-          fontWeight='bold'
-          d={props.title ? '' : 'none'}
-          background={props.background ? props.background : '#f6f2ef'}
-          position='relative'
-          zIndex='1'
-        >
-          {props.title}
-        </Box>
-        {props.children}
-      </Box>
-      <style jsx>
-        {`
-          :global(.header-line:before){
-            content: '';
-            position: absolute;
-            top: 50%;
-            left: 0;
-            right: 0;
-            width: 100%;
-            border-bottom: 1px solid gray;
-            z-index: 0;
-          }
-        `}
-      </style> */}
       <Flex justify='center'
         as='header'
         h='60px'
